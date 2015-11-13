c3b748efbba77a7a0991544a1c9b8b7b7f43c664
tbsexton <tbsexton@asu.edu>
2015-11-13 12:43:15 -0700

    new visualizations in nb

--- reg.py
+++ reg.py
@@ -19,15 +19,17 @@ class CovarianceEstimate:
         self.rem_eng = y
 
     def solve(self):
-        x0 = np.ones(self.n)
+        x0 = 1./np.ones(30)/18.927661099834339
 
-        func = lambda x:  - self.model.obj(x)
+        func = lambda x: - self.model.obj(x)
         lb = 0
         ub = 100
         bounds = [(lb, ub)]*self.n
-        print bounds
-        #res = opt.minimize(func, x0=x0, bounds=bounds, method='SLSQP')
-        res = opt.differential_evolution(func, bounds, disp=True, popsize=50)
+        # print bounds
+        print self.model.obj(x0)
+        res = opt.minimize(func, x0=x0, bounds=bounds, method='SLSQP')
+        #res = opt.differential_evolution(func, bounds, disp=True, popsize=50)
+        # res = opt.basinhopping(func, x0=x0, disp=True)
         print res.x, res.fun
         return res.x
 
@@ -36,6 +38,11 @@ pre = Preprocess(pca_model='eco_full_pca.pkl', all_dat='all_games.pkl')
 # pre.get_json('alluser_control.json')  # uncomment this to get the pkl file needed!!
 
 X, y = pre.ready_player_one(2)
+
+from sklearn.preprocessing import StandardScaler
+
+scale = StandardScaler()
+X = scale.fit_transform(X)
 
 # get sigma estimate that maximizes the sum of expected improvements
 sigma = CovarianceEstimate(X, y).solve()
@@ -47,3 +54,7 @@ with open(file_address, 'w') as f:
         pickle.dump(sigma, f)
 f.close()
 
+A = pre.pca.components_
+Std_inv = np.diag(1/scale.std_)
+vis = A.T.dot(Std_inv.dot(np.diag(sigma).dot(Std_inv.dot(A))))
+np.savetxt('visualize_this.txt', vis)
