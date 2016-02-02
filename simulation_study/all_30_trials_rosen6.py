fixed overflow issue

simulation_study/all_30_trials_rosen6.py
@@ -4,7 +4,7 @@ from ego_solver import EGO
 import numpy as np
 # from matplotlib import colors, ticker, cm
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import pandas as pd
 import pickle
 import time
@@ -37,8 +37,8 @@ for trial in range(30):
 
     ce = CovarianceEstimate(solution_X, solution_y, bounds, num_ini_guess)
     sig_scale = np.array([0.01, 0.1, 1., 10.])
-    # alpha_set = np.array([0.01, 0.1, 1., 10.])
-    alpha_set = np.array([1e-5, 1e-4, 1e-3, 1e-2])
+    alpha_set = np.array([0.01, 0.1, 1., 10.])
+    # alpha_set = np.array([1e-5, 1e-4, 1e-3, 1e-2])
     grid_result = np.zeros((sig_scale.shape[0], alpha_set.shape[0]))
     for i, s in enumerate(sig_scale):
         sig_inv = np.ones(6)*s

simulation_study/reverse_ego_obj.py
@@ -168,7 +168,7 @@ class Kriging():
         # log_prob = np.log(1./(1.+np.sum(np.exp(alpha*(sampled_path.T - path)), axis=0)))
         # sum_improv = np.sum(self.recent_path)
 
-        log_prob = np.log(np.exp(alpha*path)) - logsumexp(alpha*np.vstack((sampled_path.T, path)).T, axis=1)
+        log_prob = alpha*path - logsumexp(alpha*np.vstack((sampled_path.T, path)).T, axis=1)
         self.recent_path = log_prob
         sum_improv = np.sum(log_prob)
 